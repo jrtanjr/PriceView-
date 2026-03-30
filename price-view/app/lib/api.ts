f8f575fc2153dc26e437d7156a8e1272bb441e44
jrtanjr <jrtanjr@gmail.com>
2026-03-30 11:50:11 +0800

    update .env.local

--- price-view/app/lib/api.ts
+++ price-view/app/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://3.149.137.146:3000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export async function apiFetch(path: string, options: any = {}) {
   const token =
