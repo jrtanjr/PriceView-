bdc77dd7602852ea21ccd8e5f9823011c682b1d5
jrtanjr <jrtanjr@gmail.com>
2026-03-31 23:28:52 +0800

    update proxy.ts to remove supabase auth dependencies

--- price-view/proxy.ts
+++ price-view/proxy.ts
@@ -1,38 +1,54 @@
-import { createServerClient } from '@supabase/ssr'
+// import { createServerClient } from '@supabase/ssr' //migrate to EC2 
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+
+// export async function proxy(request: NextRequest) {
+//   console.log('proxy hit:', request.nextUrl.pathname)
+
+//   let response = NextResponse.next({
+//     request: { headers: request.headers },
+//   })
+  
+
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll: () => request.cookies.getAll(),
+//         setAll: (cookiesToSet) => {
+//           cookiesToSet.forEach(({ name, value, options }) => {
+//             request.cookies.set(name, value)
+//             response.cookies.set(name, value, options)
+//           })
+//         },
+//       },
+//     }
+//   )
+
+//   const { data: { session } } = await supabase.auth.getSession()
+
+//   if (!session) {
+//     return NextResponse.redirect(new URL('/', request.url))
+//   }
+
+//   return response
+// }
+
+// export const config = {
+//   matcher: ['/dashboard/:path*', '/details/:path*'],
+// }
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export async function proxy(request: NextRequest) {
-  console.log('proxy hit:', request.nextUrl.pathname)
+export function proxy(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
 
-  let response = NextResponse.next({
-    request: { headers: request.headers },
-  })
-  
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll: () => request.cookies.getAll(),
-        setAll: (cookiesToSet) => {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            request.cookies.set(name, value)
-            response.cookies.set(name, value, options)
-          })
-        },
-      },
-    }
-  )
-
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (!session) {
+  if (!token) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  return response
+  return NextResponse.next()
 }
 
 export const config = {
