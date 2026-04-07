8bbbc7f6607e40ec05ff6269af11063c539ced75
jrtanjr <jrtanjr@gmail.com>
2026-04-07 18:10:48 +0800

    fix middleware

--- price-view/middleware.ts
+++ price-view/middleware.ts
@@ -41,7 +41,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
 
   if (!token) {
